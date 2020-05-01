74895196467919292da1c41fd8aa3f034307068e
katesivy <58646769+katesivy@users.noreply.github.com>
2020-05-01 11:44:27 -0400

    recipes rendering

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
- import Recipes from './Recipes';
+//  import Recipes from './Recipes';
 import axios from 'axios';
 
 
@@ -16,29 +16,54 @@ class Main extends React.Component {
     async componentDidMount() {
         axios.get('http://127.0.0.1:8000/api/recipes')
             .then(response => {
-               this.setState({
-                    allRecipes: response.data.data.title
+                this.setState({
+                    allRecipes: response.data.data
                 })
                 console.log(response);
-                
+
             })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
-                    .finally(function () {
-                    });
-               
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+            });
+
     }
 
 
     render() {
         return (
-            <>
-          <Recipes /> 
-          <p> {this.state.allRecipes}</p>
-            </>
-        )
+       
+        <div className="row mt-lg-5 ml-lg-5 mr-lg-5" >
+            {this.state.allRecipes.map((allRecipes, index) => (
+           <div className="row ">
+                <div className="card text-center  "   >
+                    <div className="card-header col-12 p-5 border border-dark"  >
+               
+                        <h4>Title: {allRecipes.title}</h4>
+                        <div className="card-body" >
+                            <p className="card-text">
+                                Ingredients: {allRecipes.ingredients}
+                            </p>
+                            <p className="card-text">
+                                Directions: {allRecipes.directions}
+                            </p>
+                            <p> Nutrition Facts: {allRecipes.nutrition_facts}</p>
+                    </div>
+                
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+       
+        );
     }
+
+
+
+
+
 }
 
 export default Main;
